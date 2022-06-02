1c8c4729856c172bfab2404f1a3ecb0dff90324e
fagundesana017 <ana.fagundesluc@gmail.com>
2022-06-02 11:32:15 -0300

    Projeto filmes utilizando react

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import './style.css';
 export default function Header(){
     return(
         <header>
-          <Link className='logo' to='/'>Prime Flix</Link>
+          <Link className='logo' to='/'>PrimeFlix</Link>
           <Link className='favoritos' to='/favoritos'>Minha Lista</Link>
         </header>
     )
